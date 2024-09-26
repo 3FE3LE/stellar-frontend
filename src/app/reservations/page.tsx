ec7287eed548b0562f3e860532e00ea071e85e4d
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-09-26 14:51:54 -0500

    feat: :sparkles: get reservations feature added

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -9,37 +9,45 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { useReservationStore } from '@/store/ReservationStore';
+import { ReservationAdapter } from '@/integration/adapters';
+import { createReservationHooks } from '@/integration/hooks/ReservationHooks';
 
 export default function ReservationStatus() {
-  const { reservations } = useReservationStore();
+  const { useReservations } = createReservationHooks(ReservationAdapter);
+
+  const { results: reservations, isLoading, isError } = useReservations();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error loading trips: {isError}</div>;
 
   const totalCost = 0;
   return (
     <div className="space-y-8">
       <h2 className="text-2xl font-bold">Your Reservations</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {reservations.map((reservation) => (
-          <Card key={reservation.roomId}>
-            <CardHeader>
-              <CardTitle>{reservation.roomId} Room</CardTitle>
-              <CardDescription>
-                Reservation #{reservation.roomId}
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>
-                Check-in: {format(new Date(reservation.checkIn), "yyyy-MM-dd")}
-              </p>
-              <p>
-                Check-out:{" "}
-                {format(new Date(reservation.checkOut), "yyyy-MM-dd")}
-              </p>
-              <p>Guests: {reservation.guests}</p>
-              <p className="font-bold">Total Cost: ${totalCost}</p>
-            </CardContent>
-          </Card>
-        ))}
+        {reservations &&
+          reservations.map((reservation) => (
+            <Card key={reservation.roomId}>
+              <CardHeader>
+                <CardTitle>{reservation.roomId} Room</CardTitle>
+                <CardDescription>
+                  Reservation #{reservation.roomId}
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  Check-in:{" "}
+                  {format(new Date(reservation.checkIn), "yyyy-MM-dd")}
+                </p>
+                <p>
+                  Check-out:{" "}
+                  {format(new Date(reservation.checkOut), "yyyy-MM-dd")}
+                </p>
+                <p>Guests: {reservation.guests}</p>
+                <p className="font-bold">Total Cost: ${reservation.totalPrice}</p>
+              </CardContent>
+            </Card>
+          ))}
       </div>
     </div>
   );
