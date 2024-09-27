4b5b7a64c17510c160146f713d05640c1c90ce4b
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-09-27 18:08:23 -0500

    feat: :sparkles: Add Dark/Ligth mode toggler on app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css';
 
 import localFont from 'next/font/local';
 
+import { ThemeProvider, ThemeToggler } from '@/components';
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -29,24 +31,32 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header className="bg-blue-600 text-white p-4">
-          <h1 className="text-2xl font-bold">StellarStay Hotels</h1>
-        </header>
-        <nav className="bg-blue-500 p-4">
-          <ul className="flex space-x-4">
-            <li>
-              <a href="/" className="text-white hover:underline">
-                Search Reservations
-              </a>
-            </li>
-            <li>
-              <a href="/reservations" className="text-white hover:underline">
-                Reservation Status
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <main className="container mx-auto p-4">{children}</main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <header className="bg-blue-600 text-white p-4">
+            <h1 className="text-2xl font-bold">StellarStay Hotels</h1>
+          </header>
+          <nav className="bg-blue-500 p-4 flex justify-between">
+            <ul className="flex space-x-4">
+              <li>
+                <a href="/" className="text-white hover:underline">
+                  Search Reservations
+                </a>
+              </li>
+              <li>
+                <a href="/reservations" className="text-white hover:underline">
+                  Reservation Status
+                </a>
+              </li>
+            </ul>
+            <ThemeToggler />
+          </nav>
+          <main className="container mx-auto p-4">{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { SearchAvailable } from "./SearchAvailable";
 export { AvailableResults } from "./AvailableResults";
+
+export { ThemeProvider } from "./Providers/ThemeProvider";
+export { ThemeToggler } from "./ThemeToggler";
