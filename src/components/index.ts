5917f44fe5cae28985b3a39daeab69919796100c
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-09-28 11:29:54 -0500

    feat: :sparkles: add tooltip ink button to admin view access

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { AvailableResults } from "./AvailableResults";
 export { SearchAvailable } from "./SearchAvailable";
 export { PricingRooms } from "./PricingRooms";
+export { ToolTipLinkButton } from "./ToolTipLinkButton";
 
 export { ThemeProvider } from "./Providers/ThemeProvider";
 export { ThemeToggler } from "./ThemeToggler";
